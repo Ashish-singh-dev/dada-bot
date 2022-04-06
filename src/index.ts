8c8a9773f43758b869f9f7f6c2bb0b293752817f
Ashish Singh <ashishcbscboard@gmail.com>
2022-04-06 20:17:25 +0530

    add: express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
 import "./bot";
+import express from "express";
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.get("/", (_req, res) => {
+  res.send("Bot is running.");
+});
+
+app.listen(port, () => {
+  console.log("server started");
+});
